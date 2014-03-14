dc34e3ebdc252e2f4dbc34a77370efd4faf3de09
Asad Dhamani <dhamaniasad@gmail.com>
2014-03-14 19:59:31 +0530

    script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,63 @@
 #!/bin/bash
 
+# Install OpenVZ repo
+
+wget -P /etc/yum.repos.d/ http://ftp.openvz.org/openvz.repo
+rpm --import http://ftp.openvz.org/RPM-GPG-Key-OpenVZ
+
+# Install OpenVZ Kernel
+
+yum -y install vzkernel
+
+# Disable SELinux
+
+echo "SELINUX=disabled" > /etc/sysconfig/selinux
+
+# Install VZ Tools
+
+yum -y install vzctl
+yum -y install vzquota
+yum -y install ploop
+
+# Download templates
+
+wget -P /vz/template/cache http://download.openvz.org/template/precreated/centos-6-x86.tar.gz
+wget -P /vz/template/cache http://download.openvz.org/template/precreated/debian-7.0-x86.tar.gz
+wget -P /vz/template/cache http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
+wget -P /vz/template/cache http://download.openvz.org/template/precreated/ubuntu-13.10-x86.tar.gz
+
+# Disable iptables
+
+/etc/init.d/iptables stop
+chkconfig iptables off
+
+# System configuration
+
+sed -i 's/kernel.sysrq = 0/kernel.sysrq = 1/g' /etc/sysctl.conf
+sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+echo 'net.ipv6.conf.default.forwarding = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.default.proxy_arp = 0' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.all.rp_filter = 1' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.default.send_redirects = 1' >> /etc/sysctl.conf
+echo 'net.ipv4.conf.all.send_redirects = 0' >> /etc/sysctl.conf
+
+# Announcements
+
+echo $'OpenVZ has now been setup and configured \n'
+echo $'Four OpenVZ templates have been added to the system: \n'
+echo $'centos-6-x86 \n debian-7.0-x86 \n ubuntu-12.04-x86 \n ubuntu-13.10-x86 \n'
+
+# Reboot System
+
+$PRINTF_PATH "The system must go down for reboot now. Press Y to reboot or N to end the execution of this script."
+	read key
+	until [ "${key}" = "Y" ] || [ "${key}" = "N" ]; do
+		$PRINTF_PATH " \n Please enter a valid option"
+			read version
+    done
+	if [ "${key}" = "Y" ]; then
+reboot
+		elif [ "${key}" = "N" ]; then
+			exit 0
+      fi
